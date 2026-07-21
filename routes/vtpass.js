632a17fcb104120c25d46ca7f05f454f69cf403c
ismaildeentelecomhub <ismaildeentelecomhub@gmail.com>
2026-07-21 09:29:00 +0100

    Update vtpass.js

--- routes/vtpass.js
+++ routes/vtpass.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const router = express.Router();
 
 // =======================
-// CHECK API STATUS
+// API STATUS
 // =======================
 router.get("/balance", (req, res) => {
   res.json({
@@ -20,50 +20,6 @@ router.get("/test-auth", async (req, res) => {
   try {
     const response = await axios.get(
       "https://sandbox.vtpass.com/api/service-variations?serviceID=mtn-data",
-      {
-        headers: {
-          "api-key": process.env.VTPASS_API_KEY,
-          "public-key": process.env.VTPASS_PUBLIC_KEY,
-          "Content-Type": "application/json"
-        }
-      }
-    );
-
-    res.json(response.data);
-
-  } catch (error) {
-    console.error(error.response?.data || error.message);
-
-    res.status(500).json({
-      success: false,
-      error: error.response?.data || error.message
-    });
-  }
-});
-
-// =======================
-// BUY DATA
-// =======================
-router.post("/buy-data", async (req, res) => {
-  try {console.log(req.body);
-    const {
-      network,
-      phone,
-      variation_code,
-      amount,
-      request_id
-    } = req.body;
-
-    const response = await axios.post(
-      "https://sandbox.vtpass.com/api/pay",
-      {
-        request_id,
-        serviceID: network,
-        billersCode: phone,
-        variation_code,
-        amount,
-        phone
-      },
       {
         headers: {
           "api-key": process.env.VTPASS_API_KEY,
@@ -110,6 +66,54 @@ router.get("/data-plans/:network", async (req, res) => {
 
     res.status(500).json({
       success: false,
+      error: error.response?.data || error.message
+    });
+  }
+});
+
+// =======================
+// BUY DATA
+// =======================
+router.post("/buy-data", async (req, res) => {
+  try {
+    console.log("Request Body:", req.body);
+
+    const {
+      network,
+      phone,
+      variation_code,
+      amount,
+      request_id
+    } = req.body;
+
+    const response = await axios.post(
+      "https://sandbox.vtpass.com/api/pay",
+      {
+        request_id,
+        serviceID: network,
+        billersCode: phone,
+        variation_code,
+        amount,
+        phone
+      },
+      {
+        headers: {
+          "api-key": process.env.VTPASS_API_KEY,
+          "public-key": process.env.VTPASS_PUBLIC_KEY,
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    res.json(response.data);
+
+  } catch (error) {
+    console.error("Status:", error.response?.status);
+    console.error("Response:", error.response?.data);
+
+    res.status(500).json({
+      success: false,
+      status: error.response?.status,
       error: error.response?.data || error.message
     });
   }
